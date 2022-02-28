49789e58ead6f0ff9dba3673aba03243b8e74727
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-02-28 16:01:14 +0800

    add sass

--- Front-End/Expert/restaurant-apps/src/scripts/index.js
+++ Front-End/Expert/restaurant-apps/src/scripts/index.js
@@ -1,4 +1,2 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
-
-console.log('Hello Coders! :)');
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.scss";
