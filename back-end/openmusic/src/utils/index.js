abe1061d2d6c96c9a34e97b0c6d8b3e4181fb8e0
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-04-09 13:03:26 +0800

    create utils

---
+++ back-end/openmusic/src/utils/index.js
@@ -0,0 +1,7 @@
+const mapSongs = require('./mapSongs');
+const mapAlbums = require('./mapAlbums');
+
+module.exports = {
+    mapSongs,
+    mapAlbums,
+};
---
+++ back-end/openmusic/src/utils/mapSongs.js
@@ -0,0 +1,23 @@
+const mapSongs = ({
+    id,
+    title,
+    year,
+    genre,
+    performer,
+    duration,
+    album_id,
+    created_at,
+    updated_at,
+}) => ({
+    id,
+    title,
+    year,
+    genre,
+    performer,
+    duration,
+    albumId: album_id,
+    createdAt: created_at,
+    updatedAt: updated_at,
+});
+
+module.exports = mapSongs;
