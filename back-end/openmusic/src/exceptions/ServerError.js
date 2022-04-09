1a3621b0abd6ff030c96bb648131114de8f76cfd
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-04-09 13:42:29 +0800

    delete server error

--- back-end/openmusic/src/exceptions/ServerError.js
+++
@@ -1,9 +0,0 @@
-class ServerError extends Error {
-    constructor(message, statusCode = 500) {
-        super(message);
-        this.statusCode = statusCode;
-        this.name = 'ServerError';
-    }
-}
-
-module.exports = ServerError;
