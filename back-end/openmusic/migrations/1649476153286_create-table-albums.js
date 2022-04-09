738e639fe9b7c9f2e05be8fa77b18a695309896f
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-04-09 12:07:30 +0800

    create migrations

---
+++ back-end/openmusic/migrations/1649476153286_create-table-albums.js
@@ -0,0 +1,65 @@
+exports.up = pgm => {
+    pgm.createTable('albums', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        name: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        year: {
+            type: 'INTEGER',
+            notNull: true,
+        },
+        created_at: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        updated_at: {
+            type: 'TEXT',
+            notNull: true,
+        },
+    });
+    pgm.createTable('songs', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        title: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        year: {
+            type: 'INTEGER',
+            notNull: true,
+        },
+        genre: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        performer: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        duration: {
+            type: 'INTEGER',
+        },
+        albumId: {
+            type: 'VARCHAR(50)',
+        },
+        created_at: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        updated_at: {
+            type: 'TEXT',
+            notNull: true,
+        },
+    });
+};
+
+exports.down = pgm => {
+    pgm.dropTable('albums');
+    pgm.dropTable('songs');
+};
