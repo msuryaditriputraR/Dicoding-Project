e49ca4c40c6c9093f0942a6493afe29035b0f174
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-03-23 15:33:22 +0800

    add: migration table user

---
+++ back-end/openmusic/migrations/1679556736123_table-users.js
@@ -0,0 +1,35 @@
+exports.up = pgm => {
+    pgm.createTable('users', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        username: {
+            type: 'VARCHAR(50)',
+            unique: true,
+            notNull: true,
+        },
+        password: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        fullname: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        created_at: {
+            type: 'TIMESTAMP',
+            notNull: true,
+            default: pgm.func('current_timestamp'),
+        },
+        updated_at: {
+            type: 'TEXT',
+            notNull: true,
+            default: pgm.func('current_timestamp'),
+        },
+    });
+};
+
+exports.down = pgm => {
+    pgm.dropTable('users');
+};
