5d2584e3dddb60f3daea9519d13bb525c6843ed8
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-09-12 20:55:59 +0800

    ref logo

--- React/Dasar/react-notes/src/components/Header.jsx
+++ React/Dasar/react-notes/src/components/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
     return (
         <header className="fixed left-0 px-5 top-0 z-50 w-full shadow-sm">
             <div className="container flex justify-between items-center flex-wrap lg:flex-nowrap h-28 lg:h-16">
-                <a href="#" className="text-xl font-semibold">
+                <h1 href="#" className="text-xl font-semibold">
                     NOTES
-                </a>
+                </h1>
                 <Search />
                 <div className="flex items-center gap-x-4">
                     <div className="inline-flex items-center gap-x-1 border shadow-sm cursor-pointer hover:text-green-500 rounded-md px-3 py-2 hover:border-green-500">
