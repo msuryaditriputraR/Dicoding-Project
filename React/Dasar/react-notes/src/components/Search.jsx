0d5550c61be268d07d71efe9306e499632b9c5ed
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-09-12 20:01:53 +0800

    add: search component

---
+++ React/Dasar/react-notes/src/components/Search.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { BsSearchHeart } from "react-icons/bs";
+
+const Search = () => {
+    const [keyword, setKeyword] = useState("");
+    const handleSearch = (e) => setKeyword(e.target.value);
+
+    return (
+        <div
+            className={`group order-2 flex w-full items-center gap-x-2 rounded-md border px-4 py-2 shadow-sm transition hover:border-2 hover:border-green-500 lg:order-1 lg:w-1/2 ${
+                keyword && "border-2 border-green-500"
+            }`}
+        >
+            <input
+                type="text"
+                placeholder="Search Notes"
+                className="w-full bg-transparent caret-green-500 outline-none"
+                value={keyword}
+                onChange={handleSearch}
+            />
+            <BsSearchHeart
+                className={`text-xl group-hover:text-green-500  ${
+                    keyword && "text-green-500"
+                } `}
+            />
+        </div>
+    );
+};
+
+export default Search;
