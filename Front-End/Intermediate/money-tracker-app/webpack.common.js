53423694c87d79d838397a7c634d3d938a3c3489
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-09-30 22:43:54 +0800

    copywebpackplugin

--- Front-End/Intermediate/money-tracker-app/webpack.common.js
+++ Front-End/Intermediate/money-tracker-app/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const htmlWebpackPluginConfig = {
@@ -80,7 +81,14 @@ module.exports = {
       template: path.resolve(__dirname, 'src/views/dashboard.html'),
       ...htmlWebpackPluginConfig,
     }),
-
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
     new CleanWebpackPlugin(),
   ],
 };
