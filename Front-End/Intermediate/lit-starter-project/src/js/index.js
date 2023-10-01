6a9bfd9b188403cc5138cbb2907296ba131a5fd5
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-10-01 11:23:21 +0800

    add: todos and title app

--- Front-End/Intermediate/lit-starter-project/src/js/index.js
+++ Front-End/Intermediate/lit-starter-project/src/js/index.js
@@ -1,1 +1,32 @@
-import './components/my-element';
+import './components/title-app';
+import './components/to-dos';
+
+const todosContainer = document.querySelector('#todo-container');
+const todos = [
+  {
+    title: 'Belajar CSS Preprocessor dengan Sass',
+    completed: true,
+  },
+  {
+    title: 'Belajar CSS Framework dengan Bootstrap',
+    completed: true,
+  },
+  {
+    title: 'Belajar Lit Dasar',
+    completed: false,
+  },
+  {
+    title: 'Belajar HTTP Client dengan Axios',
+    completed: false,
+  },
+  {
+    title: 'Belajar Lit Lanjutan',
+    completed: false,
+  },
+  {
+    title: 'Belajar Firebase untuk Aplikasi Web',
+    completed: false,
+  },
+];
+
+todosContainer.setAttribute('todos', JSON.stringify(todos));
